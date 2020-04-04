459e16106c5d9d97118b856afa7b807fc59ee01c
Freddy Flores <floresfreddy.ultra@gmail.com>
2020-04-04 02:48:46 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -24,7 +24,7 @@ def return_string_array(array)
   counter = 0
   new_array = []
   while array[counter] do
-    new_array.push(array[counter])
+    new_array.push(array[counter].to_s)
     counter += 1
   end 
   new_array
